16f62ea96fc017261ad104d91c6a82f03083ffa9
Kamali <Kamalikh3@gmail.com>
2025-01-25 19:28:43 -0500

    Task 1 Store Inventory

--- cc_2.js
+++ cc_2.js
@@ -1,3 +1,7 @@
 // Task 1 Working With Arrays
-let products = ["Lightsaber", "Blue Milk", "Robe"]; // Input ["Lightsaber", "Blue Milk", "Robe"]
-console.log(products); // Output ["Lightsaber", "Blue Milk", "Robe"]
+let products = ["Lightsaber", "Blue Milk", "Robe"]; // Input ["Lightsaber", "Blue Milk", "Robe", "Boots", "Backpack"]
+console.log(products); // Output ["Lightsaber", "Blue Milk", "Robe", "Boots", "Backpack"]
+products.push("Gloves"); // Adding new product to array
+console.log(products); // Update array of products
+products.pop(); // Remove last product from array
+console.log("Updated product list:", products); // Update array of products 

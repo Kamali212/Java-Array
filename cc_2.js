18ffe76b447973dd8094c8d54006c163f51d5de4
Kamali <Kamalikh3@gmail.com>
2025-01-25 19:19:13 -0500

    Initial Commit - Object and Arrays Challenge Setup

---
+++ cc_2.js
@@ -0,0 +1,3 @@
+// Task 1 Working With Arrays
+let products = ["Lightsaber", "Blue Milk", "Robe"]; // Input ["Lightsaber", "Blue Milk", "Robe"]
+console.log(products); // Output ["Lightsaber", "Blue Milk", "Robe"]

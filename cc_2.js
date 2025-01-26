cefffa2273c4eba688454dc4b1d01a2853f421f0
Kamali <Kamalikh3@gmail.com>
2025-01-25 19:30:40 -0500

    Initial Commit - Objects and Arrays Challenge Setup

--- cc_2.js
+++ cc_2.js
@@ -1,7 +1,1 @@
-// Task 1 Working With Arrays
-let products = ["Lightsaber", "Blue Milk", "Robe"]; // Input ["Lightsaber", "Blue Milk", "Robe", "Boots", "Backpack"]
-console.log(products); // Output ["Lightsaber", "Blue Milk", "Robe", "Boots", "Backpack"]
-products.push("Gloves"); // Adding new product to array
-console.log(products); // Update array of products
-products.pop(); // Remove last product from array
-console.log("Updated product list:", products); // Update array of products 
+
